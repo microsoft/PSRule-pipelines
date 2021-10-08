c4c51f1aba15e98f7e1b4532c6ca552207c3b9aa
Bernie White <bewhite@microsoft.com>
2021-10-08 23:13:50 +1000

    Bump PSRule dependency to v1.8.0 #241 (#243)

--- pipeline.build.ps1
+++ pipeline.build.ps1
@@ -169,10 +169,10 @@ task PSRule NuGet, {
     if (!(Test-Path -Path out/dist/ps_modules)) {
         $Null = New-Item -Path out/dist/ps_modules -ItemType Directory -Force;
     }
-    if ($Null -eq (Get-InstalledModule -Name PSRule -MinimumVersion 1.7.2 -ErrorAction SilentlyContinue)) {
-        Install-Module -Name PSRule -Scope CurrentUser -MinimumVersion 1.7.2 -Force;
+    if ($Null -eq (Get-InstalledModule -Name PSRule -MinimumVersion 1.8.0 -ErrorAction SilentlyContinue)) {
+        Install-Module -Name PSRule -Scope CurrentUser -MinimumVersion 1.8.0 -Force;
     }
-    Save-Module -Name PSRule -Path out/dist/ps_modules -MinimumVersion 1.7.2;
+    Save-Module -Name PSRule -Path out/dist/ps_modules -MinimumVersion 1.8.0;
     Import-Module -Name PSRule -Verbose:$False;
 }
 
